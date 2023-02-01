8ee3fb57ebe3e451b34ebef680f1fbd47f51f236
Luciano Aguilar <lucianodavidaguilar21@gmail.com>
2023-02-01 01:08:51 -0300

    fix: resolved conflicts

--- src/app/pages/auth-login/auth-login.module.ts
+++ src/app/pages/auth-login/auth-login.module.ts
@@ -3,11 +3,9 @@ import { CommonModule } from '@angular/common';
 
 import { AuthLoginRoutingModule } from './auth-login-routing.module';
 import { LoginComponent } from './login/login.component';
-<<<<<<< HEAD
-import { FormsModule,ReactiveFormsModule } from '@angular/forms';
-=======
 
->>>>>>> 6a7e71a95fa493954f4ad60a037812dcdb3f9963
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -15,13 +13,11 @@ import { FormsModule,ReactiveFormsModule } from '@angular/forms';
   ],
   imports: [
     CommonModule,
-<<<<<<< HEAD
+
     AuthLoginRoutingModule,
     FormsModule,ReactiveFormsModule
    
-=======
-    AuthLoginRoutingModule
->>>>>>> 6a7e71a95fa493954f4ad60a037812dcdb3f9963
+
   ]
 })
 export class AuthLoginModule { }
--- src/app/pages/auth-login/login/login.component.ts
+++ src/app/pages/auth-login/login/login.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
-import {FormGroup,FormControl,FormBuilder,Validators } from '@angular/forms';
-=======
->>>>>>> 6a7e71a95fa493954f4ad60a037812dcdb3f9963
+
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
@@ -11,30 +10,25 @@ import {FormGroup,FormControl,FormBuilder,Validators } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
-<<<<<<< HEAD
-  public myForm!:FormGroup;
 
-  constructor(private fb:FormBuilder) { }
+  public myForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
 
 
   ngOnInit(): void {
-   this.myForm = this.crearFormulario();
+    this.myForm = this.crearFormulario();
   }
 
-  private crearFormulario():FormGroup{
+  private crearFormulario(): FormGroup {
     return this.fb.group({
-      email: ['',Validators.required,Validators.email],
-      password:['',Validators.required]
+      email: ['', Validators.required, Validators.email],
+      password: ['', Validators.required]
     })
   }
 
   public submitForm() {
     console.log(this.myForm.value);
-=======
-  constructor() { }
 
-  ngOnInit(): void {
->>>>>>> 6a7e71a95fa493954f4ad60a037812dcdb3f9963
   }
-
 }
