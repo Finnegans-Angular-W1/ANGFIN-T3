ad728272471a11e00bd86911b07d656c9c067670
Luciano Aguilar <lucianodavidaguilar21@gmail.com>
2023-02-01 01:39:50 -0300

    fix: resolved conflicts

--- src/app/pages/auth-login/login/login.component.ts
+++ src/app/pages/auth-login/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 
 @Component({
