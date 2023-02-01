7fc96b575db33d38cf06f5ce09d491b708cf8eca
ROBERT-Gimenez <robertgim37@gmail.com>
2023-02-01 16:57:18 -0300

    chore:Edited logo so that it is reusable and can be assigned different images when calling the component

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,19 +1,21 @@
 import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { Link } from './nav-links/nav-links.component';
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
+
 export class FooterComponent {
   navLinks: Link[] = [
     { label: 'Ig', path: '/home', icon: 'fab fa-instagram' },
     { label: 'About', path: '/about', icon: 'fas fa-info-circle' },
-    { label: 'Contact', path: '/contact', icon: 'fab fa-facebook-f' }
+    { label: 'social', path: '/contact', icon: 'fab fa-facebook-f' }
   ];
   
-  @Input() logoUrl: string ='';
+  @Input() logoUrl: string ='https://i.ytimg.com/vi/bkqI_RKS8aU/hqdefault.jpg';
 
   constructor(private router: Router) {}
 
