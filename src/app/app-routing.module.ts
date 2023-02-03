b243d4e39884175bd6d9fce844d104236eee8ac6
ROBERT-Gimenez <robertgim37@gmail.com>
2023-02-03 00:01:36 -0300

    feat:added page-not-found view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 
 import { RouterModule, Routes } from '@angular/router'; 
-
+import { PageNotFoundRoutingModule } from './pages/page-not-found/page-not-found-routing.module'; 
 const routes: Routes = [  
-
+  { path: '**', redirectTo: 'page-not-found', pathMatch: 'full' }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),PageNotFoundRoutingModule],
   exports: [RouterModule]
 })
 
