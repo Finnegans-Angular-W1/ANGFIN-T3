0926b6388bc74f0eb2dd150fc19ba4387f634100
ROBERT-Gimenez <robertgim37@gmail.com>
2023-02-07 02:00:58 -0300

    chore:code correction

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpService } from 'src/app/core/services/http.service';
-import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-home',
@@ -9,19 +7,9 @@ import { environment } from 'src/environments/environment';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private httpService:HttpService ) { }
+  constructor() { }
 
   ngOnInit(): void {
-  }
-
-  roles(){
-    const token = localStorage.getItem('token');
-    token && this.httpService.setToken(token);
-    this.httpService.get<string>(`${environment.URL_BASE}/accounts/me`).subscribe(data => {
-      console.log(data)
-    }, error => {
-      console.error(error);
-    });
   }
 
 }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PagesComponent } from './pages.component';
 import { PagesRoutingModule } from './pages-routing.module';
-import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    PagesComponent,HomeComponent
+    PagesComponent,
   ],
   imports: [
     CommonModule, // Habilita el router-outlet
