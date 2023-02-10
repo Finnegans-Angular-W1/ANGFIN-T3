bbc390eb8a25e48a3086f3ce183d11bfdd5011da
ROBERT-Gimenez <robertgim37@gmail.com>
2023-02-10 07:16:02 -0300

    fixed:error message added more especif in form

--- src/app/components/form-reset/form-reset.component.ts
+++ src/app/components/form-reset/form-reset.component.ts
@@ -1,8 +1,9 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { FormBuilder, Validators , FormControl } from '@angular/forms';
+import { FormBuilder, Validators , FormControl , FormGroup  } from '@angular/forms';
 import { environment } from 'src/environments/environment';
 import { HttpService } from 'src/app/core/services/http.service';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-form-reset',
   templateUrl: './form-reset.component.html',
@@ -13,13 +14,17 @@ export class formResetComponent implements OnInit {
   passwordVisibility = false;
   password = new FormControl('');
   userId = "";
+  userEmail="";
 
-  validateForm:any = this.fb.group({
-    password: ['', [ Validators.minLength(6) , Validators.required]]
-  })
+  validateForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email, this.checkEmail.bind(this)]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    password2: ['', [Validators.required , this.checkPassword.bind(this)]]
+  });
 
   constructor(
     private httpService:HttpService,
+    private HttpClient:HttpClient,
     private fb: FormBuilder,
     public dialogRef: MatDialogRef<formResetComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any
@@ -29,16 +34,30 @@ export class formResetComponent implements OnInit {
     this.httpService.get<any>(`${environment.URL_BASE}/auth/me`).subscribe((date:any) => {
       console.log(date)
       this.userId = date.id
+      this.userEmail = date.email
     })
   }
-
-  /* /users/resetPassword/userId */
+  checkEmail(control: FormControl): { [key: string]: boolean } | null  {
+    if (control.value === this.userEmail) {
+      return null;
+    }
+    return { checkEmail: true };
+  }
+  checkPassword(pass: FormControl): { [key: string]: boolean } | null {
+    return pass.value === this.password.value
+      ? null
+      : { mismatch: true };
+  }
+   
   submitForm() {
-    this.httpService.get<any>(`${environment.URL_BASE}/users/resetPassword/${this.userId}`).subscribe((date:any) => {
+    const data:any = {
+      email : this.validateForm.value.email ?? '',
+      password: this.validateForm.value.password ?? ''
+    }
+    console.log(data)
+    this.HttpClient.post(`${environment.URL_BASE}/users/resetPassword/${this.userId}`,data).subscribe((date:any) => {
       console.log(date)
     }) 
-    console.log( this.password.value);
-    console.log( this.userId);
   }
   
   togglePasswordVisibility() {
@@ -49,14 +68,18 @@ export class formResetComponent implements OnInit {
     this.dialogRef.close();
   }
   
-  getErrorMessage(input: string) {
-    const control = this.validateForm.controls[input];
-    if (!control) {
-      return '';
+  getErrorMessage(field: string) {
+    if (field === 'email') {
+      return this.validateForm.controls['email'].hasError('required') ? 'Email requerido' :
+        this.validateForm.controls['email'].hasError('email') ? 'Email invalido' :
+          this.validateForm.controls['email'].hasError('checkEmail') ? 'Email incorrecto' : '';
+    } else if (field === 'password') {
+      return this.validateForm.controls['password'].hasError('required') ? 'Password requerido' :
+        this.validateForm.controls['password'].hasError('minlength') ? 'Password inseguro' : '';
+    } else if (field === 'password2') {
+      return this.validateForm.controls['password2'].hasError('required') ? 'Confirme Contraseña' :
+        this.validateForm.value.password !== this.validateForm.value.password2 ? 'Las Contraseñas no coinsiden' : '';
     }
-    if (control.hasError('required')) {
-      return `${input} es requerido`;
-    }
-    return `${input} debe poseer minimo 6 caracteres`
+    return
   }
 }
