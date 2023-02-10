326357ca45db125331fdd6b32c928b1b365bbd6e
ROBERT-Gimenez <robertgim37@gmail.com>
2023-02-10 00:57:49 -0300

    chore:api call on the form

--- src/app/components/form-reset/form-reset.component.ts
+++ src/app/components/form-reset/form-reset.component.ts
@@ -1,6 +1,8 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { FormBuilder, Validators , FormControl } from '@angular/forms';
+import { environment } from 'src/environments/environment';
+import { HttpService } from 'src/app/core/services/http.service';
 @Component({
   selector: 'app-form-reset',
   templateUrl: './form-reset.component.html',
@@ -10,22 +12,25 @@ export class formResetComponent implements OnInit {
 
   passwordVisibility = false;
   inputControl = new FormControl('');
-
+  userId = "";
 
   validateForm = this.fb.group({
     password: ['', [Validators.required, Validators.minLength(6)]]
   })
 
   constructor(
+    private httpService:HttpService,
     private fb: FormBuilder,
     public dialogRef: MatDialogRef<formResetComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any
   ) { }
 
   ngOnInit() {
-    /* this.inputControl.valueChanges.subscribe(value => {
-      console.log(value);
-    }); */
+    this.httpService.get<any>(`${environment.URL_BASE}/auth/me`).subscribe((date:any) => {
+      console.log(date)
+      this.userId = date.id
+
+    })
   }
 
   togglePasswordVisibility() {
@@ -35,9 +40,13 @@ export class formResetComponent implements OnInit {
   closeModal() {
     this.dialogRef.close();
   }
-
+  /* _users_resetPassword__userId_ */
   submitForm() {
+     this.httpService.get<any>(`${environment.URL_BASE}/users/resetPassword/${this.userId}`).subscribe((date:any) => {
+      console.log(date)
+    }) 
     console.log( this.inputControl.value);
+    console.log( this.userId);
   }
 
 }
