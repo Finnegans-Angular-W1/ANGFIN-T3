9a6b5a3fbff4f6881617bf1692fbe6535a8eb5e1
Facu <facundortega1234@hotmail.com>
2023-02-15 15:49:22 -0300

    visual changes in home/ingresos/egresos

--- src/app/core/services/auth/auth.interceptor.ts
+++ src/app/core/services/auth/auth.interceptor.ts
@@ -29,41 +29,29 @@ export class AuthInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
     const token = localStorage.getItem('token');
-    if (token) {
-        const cloned = req.clone({
+
+    const reqCloned = req.clone({
           headers: req.headers.set('Authorization', `Bearer ${token}`)
-        });
-        return next.handle(cloned).pipe(
+    });
+
+    return next.handle(reqCloned).pipe(
           tap( (__) => {
             this.store.dispatch(loading({isloading:true}))
           }),
           catchError( (err:any) => {
           const msg = err.message
-          console.error(msg)
           this.openDialog()
           return throwError(msg)
           } ),
-          delay(200),
           finalize( () => 
             this.store.dispatch(loading({isloading:false}))
           )
         );
-    }
-    else {
-      return next.handle(req).pipe(
-        catchError( (err:any) => {
-        const msg = err.message
-        console.error(msg)
 
-        this.openDialog()
-
-        return throwError(msg)
-        } )
-      );
-    }
   }
 
 }
+
 
 
 
