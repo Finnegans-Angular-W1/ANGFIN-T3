308e48a6339f8bfe969df08869d48e77beeb547b
Luciano Aguilar <lucianodavidaguilar21@gmail.com>
2023-02-16 00:51:20 -0300

    nuevas pruebas test de servicio

--- src/app/pages/auth/registro/registro.component.spec.ts
+++ src/app/pages/auth/registro/registro.component.spec.ts
@@ -1,38 +1,44 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HttpErrorResponse } from '@angular/common/http';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { Form, FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { ComponentFixtureAutoDetect } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { provideMockStore } from '@ngrx/store/testing';
 
 import { RegistroComponent } from './registro.component';
 import { AuthService } from 'src/app/core/services/auth/auth.service';
+import { of, throwError } from 'rxjs';
+import { Store } from '@ngrx/store';
 
 
 describe('RegistroComponent', () => {
   let component: RegistroComponent;
   let fixture: ComponentFixture<RegistroComponent>;
-  const formBuilder : FormBuilder=new FormBuilder();
-  let service:AuthService;
+  const formBuilder: FormBuilder = new FormBuilder();
+  let service: AuthService;
+  let httpClientSpy: { post: jasmine.Spy }
+  let store:Store;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [RegistroComponent],
-      imports: [ReactiveFormsModule, HttpClientModule],
-      providers: [provideMockStore({}), { provide: ComponentFixtureAutoDetect, useValue: true },AuthService],
+      imports: [ReactiveFormsModule, HttpClientModule, HttpClientTestingModule],
+      providers: [provideMockStore({}), { provide: ComponentFixtureAutoDetect, useValue: true }, AuthService],
     }).compileComponents();
 
 
   });
 
   beforeEach(() => {
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['post'])
+    service = new AuthService(httpClientSpy as any,store)
 
     fixture = TestBed.createComponent(RegistroComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   })
 
-  it('should create', () => {
+  it('Debe crearse', () => {
 
     expect(component).toBeTruthy();
   });
@@ -70,39 +76,33 @@ describe('RegistroComponent', () => {
   })
 
   it('validando usuario', () => {
-    component.registroForm = formBuilder.group( {
-      first_name:['Juan'],
-      last_name:['Perez'],
-      email:['test@test.com'],
-      password:['test123'],
-      terms:[true]
+    component.registroForm = formBuilder.group({
+      first_name: ['Juan'],
+      last_name: ['Perez'],
+      email: ['test@test.com'],
+      password: ['test123'],
+      terms: [true]
     })
-    
+
     expect(component.registroForm.valid).toEqual(true)
     expect(component.canSubmit()).toEqual(true)
   })
 
-  it('usuario invalido con email incompleto', () => {
-    // component.registroForm = formBuilder.group( {
-    //   first_name:['Juan'],
-    //   last_name:['Perez'],
-    //   email:[''],
-    //   password:['test123'],
-    //   terms:[true]
-    // })
+  it('usuario invalido con email vacio', () => {
+    
     component.registroForm.value.first_name = 'Juan'
     component.registroForm.value.last_name = 'Perez'
-    component.registroForm.value.email=''
+    component.registroForm.value.email = ''
     component.registroForm.value.password = 'abc123'
     expect(component.registroForm.valid).toEqual(false)
     expect(component.canSubmit()).toEqual(false)
   })
 
   it('usuario invalido con email incompleto', () => {
-    
+
     component.registroForm.value.first_name = 'Juan'
     component.registroForm.value.last_name = 'Perez'
-    component.registroForm.value.email='juan@example'
+    component.registroForm.value.email = 'juan@example'
     component.registroForm.value.password = 'abc123'
     expect(component.registroForm.valid).toEqual(false)
     expect(component.canSubmit()).toEqual(false)
@@ -111,25 +111,65 @@ describe('RegistroComponent', () => {
   it('usuario invalido con password incompleto', () => {
     component.registroForm.value.first_name = 'Juan'
     component.registroForm.value.last_name = 'Perez'
-    component.registroForm.value.email='asd@asd.asd'
+    component.registroForm.value.email = 'asd@asd.asd'
     component.registroForm.value.password = ''
     expect(component.canSubmit()).toEqual(false)
   })
 
-  // it('test servicio register',()=>{
-  //   component.registroForm = formBuilder.group( {
-  //     first_name:['Juan'],
-  //     last_name:['Perez'],
-  //     email:['asd@asd.com'],
-  //     password:['test123'],
-  //     terms:[true]
-  //   });
+  it('test servicio register', (done: DoneFn) => {
+    component.registroForm = formBuilder.group({
+      first_name: ['Juan'],
+      last_name: ['Perez'],
+      email: ['asd@asd.com'],
+      password: ['test123'],
+      terms: [true]
+    });
 
-  //   service.register(component.registroForm).subscribe(
-      
-  //   )
-  // })
+    const credencialesResult = {
+      "first_name": "Juan",
+      "last_name": "Perez",
+      "email": "asd@asd.com",
+      "password": "test123",
+      "roleId": 2,
+      "points": 50
+    }
+
+    httpClientSpy.post.and.returnValue(of(credencialesResult))
 
 
-  
+    service.register(component.registroForm).subscribe(res =>{
+      expect(res).toEqual(credencialesResult)
+      done()
+    })
+
+  })
+
+  it('test el usuario ya existe', (done: DoneFn) => {
+    component.registroForm = formBuilder.group({
+      first_name: ['Juan'],
+      last_name: ['Perez'],
+      email: ['juanperez@example.com'],
+      password: ['test123'],
+      terms: [true]
+    });
+
+    const error500 = new HttpErrorResponse( {
+      "error": "Error desconocido",
+      "status": 500
+    })
+    
+    httpClientSpy.post.and.returnValue(throwError(error500))
+
+
+    service.register(component.registroForm).subscribe({
+      next:(res:any) =>{
+
+      },error:err=>{
+      expect(err.status).toEqual(500)
+      done()
+    }})
+
+  })
+
+
 });
