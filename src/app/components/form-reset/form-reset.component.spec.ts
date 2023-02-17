be1fac8a23d5178cc07644695eeeaa07ce66a990
ROBERT-Gimenez <robertgim37@gmail.com>
2023-02-17 02:14:05 -0300

    chore:add animation in title reusable

--- src/app/components/form-reset/form-reset.component.spec.ts
+++ src/app/components/form-reset/form-reset.component.spec.ts
@@ -1,21 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
-import { By } from '@angular/platform-browser';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 import { formResetComponent } from './form-reset.component';
+import { MatDialogModule } from '@angular/material/dialog';
 
-/* 
-describe('formResetComponent', () => {
+
+
+describe('FormResetComponent', () => {
   let component: formResetComponent;
   let fixture: ComponentFixture<formResetComponent>;
-  
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ReactiveFormsModule],
-      declarations: [formResetComponent],
-    }).compileComponents();
-  });
+      imports: [HttpClientTestingModule, ReactiveFormsModule,MatDialogModule], 
+      declarations: [ formResetComponent ]
+    })
+    .compileComponents();
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(formResetComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -24,57 +25,4 @@ describe('formResetComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-
-  it('should disable submit button when form is invalid', () => {
-    const emailControl = component.validateForm.get('email') as FormControl;
-    emailControl.setValue('');
-
-    const passwordControl = component.validateForm.get('password') as FormControl;
-    passwordControl.setValue('password');
-
-    const password2Control = component.validateForm.get('password2') as FormControl;
-    password2Control.setValue('password');
-
-    fixture.detectChanges();
-
-    const submitButton = fixture.debugElement.query(By.css('button[type="submit"]')).nativeElement;
-    expect(submitButton.disabled).toBeTruthy();
-  });
-
-  it('should enable submit button when form is valid', () => {
-    const emailControl = component.validateForm.get('email') as FormControl;
-    emailControl.setValue('test@example.com');
-
-    const passwordControl = component.validateForm.get('password') as FormControl;
-    passwordControl.setValue('password');
-
-    const password2Control = component.validateForm.get('password2') as FormControl;
-    password2Control.setValue('password');
-
-    fixture.detectChanges();
-
-    const submitButton = fixture.debugElement.query(By.css('button[type="submit"]')).nativeElement;
-    expect(submitButton.disabled).toBeFalsy();
-  });
-
-  it('should call submitForm function when form is submitted', () => {
-    spyOn(component, 'submitForm');
-
-    const emailControl = component.validateForm.get('email') as FormControl;
-    emailControl.setValue('test@example.com');
-
-    const passwordControl = component.validateForm.get('password') as FormControl;
-    passwordControl.setValue('password');
-
-    const password2Control = component.validateForm.get('password2') as FormControl;
-    password2Control.setValue('password');
-
-    fixture.detectChanges();
-
-    const submitButton = fixture.debugElement.query(By.css('button[type="submit"]')).nativeElement;
-    submitButton.click();
-
-    expect(component.submitForm).toHaveBeenCalled();
-    
-  });
-}); */
+});
