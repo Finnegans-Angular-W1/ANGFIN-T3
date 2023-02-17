cb9d87e05c3f7c7b70b603b545ad731978562941
Facu <facundortega1234@hotmail.com>
2023-02-17 16:05:28 -0300

    antes de map

--- src/app/pages/wallet/wallet.component.ts
+++ src/app/pages/wallet/wallet.component.ts
@@ -7,9 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WalletComponent implements OnInit {
 
+  canSee : boolean = false;
+
+  canViewForm:boolean = false
+
+  numberOperations :number = 10;
+
+  info! : string 
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  changeView(){
+    this.canSee = !this.canSee
+  }
+
+  addEvent(date:string){
+
+    if(!date){
+      return
+    }
+
+    this.closedForm()
+
+    this.numberOperations = Math.round(Math.random() * 100000)
+
+
+  }
+
+  openForm(text :string){
+    this.canViewForm = true
+    this.info = text
+  }
+
+  closedForm(){
+    this.canViewForm = false
   }
 
 }
